c40ad345d3cd3e12bb4493953d852751ffc7f219
alexanderguo <alexanderguo@welove-inc.com>
2021-08-02 13:57:58 +0800

    解析宏观/微观日志的脚本

--- src/main/scala/com/gpy/spark/batch/ParseMicLog.scala
+++ src/main/scala/com/gpy/spark/batch/ParseMicLog.scala
@@ -14,35 +14,32 @@ object ParseMicLog {
     //1.构建本地测试环境
     val spark = SparkSession
       .builder()
-      .master("local[*]")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .appName("testDemo")
       .getOrCreate()
     val sc = spark.sparkContext
     import spark.implicits._
-    val user_info_file =
-      "file:///D:\\IdeaProjects\\SparkAPI\\data\\welove.log_2020-01-19.log.merge"
-    val outPutPath = ""
+    val user_info_file = args(0)
+    val outPutPath = args(1)
     val df = spark.read
       .textFile(user_info_file)
-      .repartition(200)
+      .repartition(100)
       .filter(line => !line.contains("Microscopic_view"))
       .map { line =>
         val arr = line.split(" ")
         val date = arr(0)
-        val ip_1 = arr(2)
-        val ip_2 = arr(3)
+        val ip = arr(3)
         val user_id = arr(4)
         val space_id = arr(5)
-        (date, user_id, ip_1, ip_2)
+        (date, user_id, ip)
       }
-      .toDF("date", "user_id", "ip_1", "ip_2")
+      .toDF("date", "user_id", "ip")
       .where("user_id is not null")
-      .groupBy("date", "user_id", "ip_1", "ip_2")
+      .groupBy("date", "user_id", "ip")
       .count()
       .alias("ip_num_cnt")
-      .orderBy("date")
-    df.show(100)
+      .orderBy("date", "user_id")
+    df.coalesce(1).write.csv(outPutPath)
 
     val end_time = System.currentTimeMillis()
     println("time_cost: " + (end_time - start_time) / 1000 + " sec")
